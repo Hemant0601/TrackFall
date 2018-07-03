96a886c25f89a3372945dbc7144ba1fea9b0cbb9
Hemant <hemant.kumar@orahi.com>
2018-07-03 18:41:19 +0530

    git changes updated

--- app/src/main/java/com/hks/trackfall/MainActivity.java
+++ app/src/main/java/com/hks/trackfall/MainActivity.java
@@ -1,14 +1,22 @@
 package com.hks.trackfall;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -16,13 +24,14 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private TextView state;
     private SensorManager sensorManager;
     private double FREE_FALL_DELTA = 3.0;
-
+    static final Integer CALL = 0x2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setSupportActionBar(toolbar);
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         state = (TextView) findViewById(R.id.state);
+        hasPhonePermission();
 
     }
 
@@ -50,13 +60,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_call:
+                //open settings for calling feature
+                redirectToCallSettings();
+                break;
+
         }
+
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void redirectToCallSettings() {
+    }
 
 
     @Override
@@ -75,11 +92,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         double acceleration = Math.sqrt((x * x) + (y * y) + (z * z)); //generally remains around 9.8 m/s
 
-        if(acceleration<FREE_FALL_DELTA) {
+        if (acceleration < FREE_FALL_DELTA) {
             Log.v("DETECTING FREE FALL", acceleration + "");
             state.setText("FALL DETECTED");
             state.setTextColor(Color.RED);
             resetState();
+            callPhone();
         }
     }
 
@@ -92,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 state.setTextColor(Color.GREEN);
 
             }
-        },5000);
+        }, 5000);
     }
 
 
@@ -123,5 +141,76 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onPointerCaptureChanged(boolean hasCapture) {
 
     }
+
+
+    void checkPermission(String permission, Integer requestCode) {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, permission) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, permission)) {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+
+            } else {
+
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{permission}, requestCode);
+            }
+        } else {
+            Toast.makeText(this, "" + permission + " is already granted.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void hasPhonePermission() {
+        checkPermission(Manifest.permission.CALL_PHONE, CALL);
+        ;
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (ActivityCompat.checkSelfPermission(this, permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            switch (requestCode) {
+                case 2:
+
+                    break;
+
+            }
+
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    void callPhone() {
+
+
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:" + "1234567890"));
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+            startActivity(callIntent);
+
+        }else {
+            if (this.checkSelfPermission(Manifest.permission.CALL_PHONE) == PackageManager.PERMISSION_GRANTED) {
+                startActivity(callIntent);
+            }else {
+            }
+        }
+
+    }
+
+
+
 }
 
